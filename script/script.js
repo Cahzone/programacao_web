01b988ca8f866bf64fae93ee88839ab1801690f8
Cahzone <60630634+Cahzone@users.noreply.github.com>
2020-02-12 21:33:26 -0300

    login JS2

---
+++ script/script.js
@@ -0,0 +1,31 @@
+banco de dados
+
+// loginEl=Carla
+// senhaEl=123
+
+var app = document.getElementById("app")
+
+
+
+var loginEl = document.createAttribute("imput")
+loginEl.setattribute("type","text")
+loginEl.setattribute("placeholder","Escreva seu nome")
+loginEl.setattribute("id","usuario")
+app.appendChild(loginEl)
+
+var senhaEl = document.createAttribute("imput")
+senhaEl.setattribute("type","text")
+senhaEl.setattribute("placeholder","Escreva sua senha")
+senhaEl.setattribute("id","usuario")
+app.appendChild(senhaEl)
+
+// var entrarE1
+
+
+// function checarlogin() {
+    // if((loginEl.value === usuarioAdmin)
+    // && (senhaEl.value === senhaAdmin))
+// else = senao
+
+
+}

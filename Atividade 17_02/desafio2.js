5ebd0220ab75866ec793e19e1f7f3baa7616137d
Cahzone <60630634+Cahzone@users.noreply.github.com>
2020-02-17 21:30:21 -0300

    desafios 1 e 2

---
+++ Atividade 17_02/desafio2.js
@@ -0,0 +1,20 @@
+function divisao(a,b){
+    return a + b }
+    var total = soma (2, 3) + 5
+
+    total
+
+var nova
+    function nova(a){
+    return a = "Valor"}
+
+    nova('teste')
+    "Valor"
+
+    //function nova(a){
+    //return a = "Valor"}
+
+    
+
+
+
